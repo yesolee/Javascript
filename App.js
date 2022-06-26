08f6a31411e63d135ff3b2ca257ad294bfd5eeeb
yesolee <58902084+yesolee@users.noreply.github.com>
2022-06-26 20:42:10 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -22,6 +22,14 @@ function App() {
       <div className='black-nav'>
         <h4 style={{ fontSize: '16px' }}>ReactBlog</h4>
       </div>
+      <button
+        onClick={() => {
+          let copy2 = [...글제목];
+          글제목변경(copy2.sort());
+        }}
+      >
+        가나다순정렬
+      </button>
       <div className='list'>
         <h4>
           {글제목[0]}{' '}
@@ -38,7 +46,7 @@ function App() {
           2월 17일 발행
           <span
             onClick={() => {
-              let copy = [...글제목];
+              let copy = [...글제목]; // 그냥 글제목이라고 하면 참조값이 할당되는거라서 변경X
               copy[0] = '여자 코트 추천';
               글제목변경(copy);
             }}
@@ -55,8 +63,30 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal></Modal>
+      <Weather></Weather>
     </div>
   );
 }
 
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
+
+function Weather() {
+  return (
+    <div className='weather'>
+      <h3>지역</h3>
+      <p>기온</p>
+      <p>강수확율</p>
+    </div>
+  );
+}
 export default App;
