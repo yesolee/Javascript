de1b8c3092dab76e11ad5774166cdddc8ed6d5ad
yesolee <58902084+yesolee@users.noreply.github.com>
2022-06-27 20:32:15 +0900

    setting

---
+++ React/voca/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Hello from './component/Hello';
+import Welcome from './component/Welcome';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Hello />
+    </div>
+  );
+}
+////boolin type이나 객체는{변수}문법 X
+export default App;
