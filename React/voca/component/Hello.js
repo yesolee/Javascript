a61f8a65b51f02e147fd1a83807c48eeb521c554
yesolee <58902084+yesolee@users.noreply.github.com>
2022-06-27 21:43:50 +0900

    props

--- React/voca/component/Hello.js
+++ React/voca/component/Hello.js
@@ -1,11 +1,16 @@
 import { useState } from 'react';
+import UserName from './UserName';
 
-export default function Hello() {
+export default function Hello({ age }) {
   const [name, setName] = useState('Mike');
+  const msg = age > 19 ? '성인입니다.' : '미성년자 입니다.';
 
   return (
     <div>
-      <h2>{name}</h2>
+      <h2>
+        {name}({age}) : {msg}
+      </h2>
+      <UserName name={name} />
       <button
         onClick={() => {
           setName(name === 'Mike' ? 'Jane' : 'Mike');
