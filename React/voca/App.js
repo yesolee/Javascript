317dbe51d13165cc8ad01ed3ee5c22275b746e34
yesolee <58902084+yesolee@users.noreply.github.com>
2022-06-27 21:43:06 +0900

    props

--- React/voca/App.js
+++ React/voca/App.js
@@ -6,9 +6,9 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className='App'>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello age={10} />
+      <Hello age={20} />
+      <Hello age={30} />
     </div>
   );
 }
