712f9f258011e8aa59727820f220bd0c6a04fea0
yesolee <58902084+yesolee@users.noreply.github.com>
2022-06-27 21:16:37 +0900

    useState

--- React/voca/component/Hello.js
+++ React/voca/component/Hello.js
@@ -1,43 +1,18 @@
-// const Hello = () => {
-//   <p>Hello</p>;
-// };
-
-// export default Hello;
-import World from './World';
-import styles from './Hello.module.css';
+import { useState } from 'react';
 
 export default function Hello() {
-  function showName() {
-    document.getElementById('title').innerHTML = 'Mike';
-  }
-
-  function showAge(txt) {
-    console.log(txt);
-  }
-
-  function showText(txt) {
-    console.log(txt);
-  }
+  const [name, setName] = useState('Mike');
 
   return (
     <div>
-      <h1 id='title'>Hello</h1>
-      <button onClick={showName}>Show name</button>
+      <h2>{name}</h2>
       <button
-        onClick={(e) => {
-          const txt = e.target.value;
-          showText(txt);
+        onClick={() => {
+          setName(name === 'Mike' ? 'Jane' : 'Mike');
         }}
       >
-        Show age
+        change
       </button>
-      <input
-        type='text'
-        onChange={(e) => {
-          const txt = e.target.value;
-          showText(txt);
-        }}
-      ></input>
     </div>
   );
 }
