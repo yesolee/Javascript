6a7260c883cf825a570722dc2c0720f985f40982
yesolee <58902084+yesolee@users.noreply.github.com>
2022-06-29 23:00:08 +0900

    Fetch

--- React/voca/component/Day.js
+++ React/voca/component/Day.js
@@ -1,20 +1,19 @@
-import dummy from '../db/data.json';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Word from './Word';
+import useFetch from '../hooks/useFetch';
 
 export default function Day() {
   const { day } = useParams();
-  const wordList = dummy.words.filter((word) => word.day === Number(day));
+  const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
-            <tr key={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+          {words.map((word) => (
+            <Word word={word} key={word.id} />
           ))}
         </tbody>
       </table>
--- React/voca/component/DayList.js
+++ React/voca/component/DayList.js
@@ -1,39 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import useFetch from '../hooks/useFetch';
 
 export default function DayList() {
-  const [days, setDays] = useState([]);
-  const [count, setCount] = useState(0);
-
-  function onClick() {
-    setCount(count + 1);
-  }
-
-  function onClick2() {
-    setDays([
-      ...days,
-      {
-        id: Math.random(),
-        day: 1,
-      },
-    ]);
-  }
-
-  useEffect(() => {
-    console.log('Count change');
-  }, []);
+  const days = useFetch('http://localhost:3001/days');
 
   return (
-    <>
-      <ul className='list_day'>
-        {days.map((day) => (
-          <li key={day.id}>
-            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-          </li>
-        ))}
-      </ul>
-      <button onClick={onClick}></button>
-      <button onClick={onClick2}>Day change</button>
-    </>
+    <ul className='list_day'>
+      {days.map((day) => (
+        <li key={day.id}>
+          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+        </li>
+      ))}
+    </ul>
   );
 }
