061a6e50ed822f2c26888163e812695c402610cc
yesolee <58902084+yesolee@users.noreply.github.com>
2022-06-29 20:46:10 +0900

    REST API & useEffect

--- React/voca/component/DayList.js
+++ React/voca/component/DayList.js
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import dummy from '../db/data.json';
 
 export default function DayList() {
+  const [days, setDays] = useState([]);
+  const [count, setCount] = useState(0);
+
+  function onClick() {
+    setCount(count + 1);
+  }
+
+  function onClick2() {
+    setDays([
+      ...days,
+      {
+        id: Math.random(),
+        day: 1,
+      },
+    ]);
+  }
+
+  useEffect(() => {
+    console.log('Count change');
+  }, []);
+
   return (
-    <ul className='list_day'>
-      {dummy.days.map((day) => (
-        <li key={day.id}>
-          <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className='list_day'>
+        {days.map((day) => (
+          <li key={day.id}>
+            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+          </li>
+        ))}
+      </ul>
+      <button onClick={onClick}></button>
+      <button onClick={onClick2}>Day change</button>
+    </>
   );
 }
