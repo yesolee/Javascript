523785c98240f5554efa301cb740418e02d9a42d
yesolee <58902084+yesolee@users.noreply.github.com>
2022-07-03 23:36:51 +0900

    POST

---
+++ React/voca/component/CreateWord.js
@@ -0,0 +1,33 @@
+import useFetch from '../hooks/useFetch';
+
+export default function CreateWord() {
+  const days = useFetch('http://localhost:3001/days');
+
+  function onSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className='input_area'>
+        <label>Eng</label>
+        <input type='text' placeholder='computer' />
+      </div>
+      <div className='input_area'>
+        <label>Kor</label>
+        <input type='text' placeholder='컴퓨터' />
+      </div>
+      <div className='input_area'>
+        <label>Day</label>
+        <select>
+          {days.map((day) => {
+            <option key={day.id} value={day.day}>
+              {day.day}
+            </option>;
+          })}
+        </select>
+      </div>
+      <button>저장</button>
+    </form>
+  );
+}
