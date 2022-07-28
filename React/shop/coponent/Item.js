fe062bb4dc7f14efdcf118ea24b937084286c372
yesolee <58902084+yesolee@users.noreply.github.com>
2022-07-28 23:12:55 +0900

    최근 본 상품

--- React/shop/coponent/Item.js
+++ React/shop/coponent/Item.js
@@ -3,14 +3,19 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Item(props) {
   let navigate = useNavigate();
-
   return (
     <Col
       onClick={() => {
         navigate('/detail/' + props.cocktail.id);
+        let getWatched = JSON.parse(localStorage.getItem('watched'));
+        getWatched.unshift(props.cocktail.id);
+        localStorage.setItem('watched', JSON.stringify(getWatched));
       }}
     >
-      <img src={props.cocktail.link} width='80%' height='320vw'></img>
+      <div width='1504px'>
+        <img src={props.cocktail.link} width='100%' height='320vw'></img>
+      </div>
+
       <h4>{props.cocktail.title}</h4>
       <p>{props.cocktail.price}</p>
     </Col>
